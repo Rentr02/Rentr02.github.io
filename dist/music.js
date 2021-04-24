8f180926f7c360430462814d0440847c316870d8
P-tow-K <68007464+P-tow-K@users.noreply.github.com>
2021-04-24 19:32:52 +0800

    Site updated: 2021-04-24 19:32:51

--- dist/music.js
+++ dist/music.js
@@ -1,11 +1,11 @@
 const ap = new APlayer({
     container: document.getElementById('player'),
-    autoplay: false,
+    autoplay: true,
     fixed: true,
     audio: [{
         name: '以父之名',
         artist: 'jay',
         url: 'http://www.ytmp3.cn/down/49201.mp3',
-        cover: 'cover.jpg',
+        cover: '1.png',
     }]
 });
